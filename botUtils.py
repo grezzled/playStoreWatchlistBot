16de4258932071235a55b4bef796e58595f15e0d
grezzled <belchhabsoufiane.official@gmail.com>
2023-04-13 17:23:29 +0000

    clean code & add revenue cmd

---
+++ botUtils.py
@@ -0,0 +1,72 @@
+import os
+
+import schedule
+import telebot
+
+from api.applovinMax import applovinMax
+from db.dbConfig import dbConfig
+from scraper.gpScraper import scrap_app
+
+applovin_report_key = os.getenv('APPLOVIN_REPORT_KEY')
+
+cmds = [
+    telebot.types.BotCommand("start", "Welcoming message"),
+    telebot.types.BotCommand("addapp", "Add an app"),
+    telebot.types.BotCommand("addapps", "Add multiple apps at once"),
+    telebot.types.BotCommand("myapps", "Show the list of your apps"),
+    telebot.types.BotCommand("statusapps", "Check status of your apps"),
+    telebot.types.BotCommand("startnotifier", "Turn on notifications to receive updates"),
+    telebot.types.BotCommand("stopnotifier", "Turn off notifications to stop receiving updates"),
+    telebot.types.BotCommand("cancel", "Cancel the current operation"),
+    telebot.types.BotCommand("revenue", "Get details about your revenue"),
+    telebot.types.BotCommand("help", "Instructions for using Bot commands"),
+]
+
+
+def build_cmds(bot, debug=False):
+    if debug:
+        bot.set_my_commands(cmds)
+
+
+def notifier(message):
+    if not schedule.get_jobs(message.chat.id):
+        schedule.every(1).hour.do(status_notifier, message.chat.id).tag(message.chat.id)
+        schedule.every(30).minutes.do(revenue_notifier, message.chat.id).tag(message.chat.id)
+
+
+def revenue_details(bot, message):
+    _msg = bot.send_message(chat_id=message.chat.id, text='Loading...', parse_mode='HTML')
+    alm = applovinMax(applovin_report_key)
+    today_revenue = alm.today_revenue()
+    yesterday_revenue = alm.yesterday_revenue()
+    if today_revenue and yesterday_revenue:
+        today_revenue = round(float(today_revenue), 2)
+        yesterday_revenue = round(float(yesterday_revenue), 2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=_msg.message_id,
+                          text=f'💰Yesterday💰: <b>{yesterday_revenue}$</b>'
+                               f' \n\n💰<b>Today</b>💰: <b>{today_revenue}$</b>',
+                          parse_mode='HTML')
+
+
+def status_notifier(bot, message) -> None:
+    pkgs = dbConfig().get_pkgs(int(message.chat.id))
+    if len(pkgs) <= 0:
+        bot.send_message(message.chat.id, "You have no apps yet, please use the command /addapp the add a new app.")
+    else:
+        for pkg in pkgs:
+            if scrap_app(pkg, message.chat.id) is not False:
+                bot.send_message(message.chat.id, f'🟢 {pkg}')
+            else:
+                bot.send_message(message.chat.id, f'🔴 {pkg}')
+
+
+def revenue_notifier(bot, message):
+    alm = applovinMax(applovin_report_key)
+    today_revenue = alm.today_revenue()
+    yesterday_revenue = alm.yesterday_revenue()
+    if today_revenue and yesterday_revenue:
+        today_revenue = round(float(today_revenue), 2)
+        yesterday_revenue = round(float(yesterday_revenue), 2)
+    bot.send_message(message.chat.id, f'💰Yesterday💰: <b>{yesterday_revenue}$</b>'
+                                      f' \n\n💰<b>Today</b>💰: <b>{today_revenue}$</b>',
+                     parse_mode='HTML')
--- main.py
+++ main.py
@@ -3,71 +3,71 @@ import schedule
 from dotenv import load_dotenv
 from telebot import *
 from telebot.async_telebot import *
-from telebot.handler_backends import StatesGroup
-
 import constants
 from botFactories import pkgs_factory, delpkg_factory
 from botFilters import pkgsCallbackFilter, delPkgCallbackFilter
 from botKeyboards import pkgs_keyboard, options_keyboard
 from botSteps import process_pkg_step, process_pkgs_step
 from db.dbConfig import dbConfig, build_db
-from scraper.gpScraper import scrap_app
+from botUtils import revenue_details, status_notifier, notifier, build_cmds
 
-from api.applovinMax import applovinMax
-
-""" Load environment variables """
 load_dotenv()
 
-""" Create database and it's tables if it's not already created """
 build_db()
-
-""" Initialize the bot commands """
-cmds = [
-    telebot.types.BotCommand("start", "Welcoming message"),
-    telebot.types.BotCommand("addapp", "Add an app"),
-    telebot.types.BotCommand("addapps", "Add multiple apps at once"),
-    telebot.types.BotCommand("myapps", "Show the list of your apps"),
-    telebot.types.BotCommand("statusapps", "Check status of your apps"),
-    telebot.types.BotCommand("cancel", "Cancel the current operation"),
-    telebot.types.BotCommand("startnotifier", "Turn on notifications to receive updates"),
-    telebot.types.BotCommand("stopnotifier", "Turn off notifications to stop receiving updates"),
-    telebot.types.BotCommand("help", "Instructions for using Bot commands"),
-
-]
-
-applovin_report_key = os.getenv('APPLOVIN_REPORT_KEY')
 
 bot = TeleBot(token=os.getenv('API_TOKEN'))
 
-bot.set_my_commands(cmds)
+build_cmds(bot, False)
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def cmd_start(message):
     print(f'Count of running jobs: {len(schedule.get_jobs())}')
     bot.send_message(chat_id=message.chat.id, text=constants.START, parse_mode='HTML')
     notifier(message)
 
 
 @bot.message_handler(commands=['addapp'])
-def add_pkg(message):
+def cmd_add_app(message):
     bot.send_message(message.chat.id, constants.ENTER_PKG_NAME, parse_mode='HTML')
     bot.register_next_step_handler(message=message, callback=process_pkg_step, _bot=bot)
 
 
 @bot.message_handler(commands=['addapps'])
-def add_pkgs(message):
+def cmd_add_apps(message):
     bot.send_message(message.chat.id, constants.ENTER_PKGS_NAMES, parse_mode='HTML')
     bot.register_next_step_handler(message=message, callback=process_pkgs_step, _bot=bot)
 
 
 @bot.message_handler(commands=['myapps'])
-def list_pkgs(message):
+def cmd_list_apps(message):
     pkgs = dbConfig().get_pkgs(int(message.chat.id))
     if len(pkgs) <= 0:
         bot.send_message(message.chat.id, constants.YOU_HAVE_NO_APPS, parse_mode='HTML')
     else:
         bot.send_message(message.chat.id, constants.SELECT_APP, parse_mode='HTML', reply_markup=pkgs_keyboard(pkgs))
+
+
+@bot.message_handler(commands=['statusapps'])
+def status_pkgs(message):
+    status_notifier(bot, message)
+
+
+@bot.message_handler(commands=['stopnotifier'])
+def stop_notifier(message):
+    schedule.clear(message.chat.id)
+    bot.send_message(message.chat.id, constants.NOTIFIER_STOPPED, parse_mode='HTML')
+
+
+@bot.message_handler(commands=['revenue'])
+def revenue(message):
+    revenue_details(bot, message)
+
+
+@bot.message_handler(commands=['startnotifier'])
+def start_notifier(message):
+    notifier(message)
+    bot.send_message(message.chat.id, constants.NOTIFIER_STARTED, parse_mode='HTML')
 
 
 @bot.callback_query_handler(func=None, config=pkgs_factory.filter())
@@ -85,12 +85,6 @@ def back_callback(call: types.CallbackQuery):
                           text=constants.YOUR_APPLICATIONS, parse_mode='HTML', reply_markup=pkgs_keyboard(pkgs))
 
 
-@bot.message_handler(commands=['addapps'])
-def add_pkg(message):
-    bot.send_message(message.chat.id, "Please enter your apps package names separated by a comma (,)")
-    bot.register_next_step_handler(message=message, callback=process_pkg_step, _bot=bot)
-
-
 @bot.callback_query_handler(func=None, delpkg=delpkg_factory.filter())
 def delpkg_callback(call: types.CallbackQuery):
     callback_data: dict = delpkg_factory.parse(callback_data=call.data)
@@ -102,26 +96,9 @@ def delpkg_callback(call: types.CallbackQuery):
                           text=constants.SELECT_APP, parse_mode='HTML', reply_markup=pkgs_keyboard(pkgs))
 
 
-@bot.message_handler(commands=['statusapps'])
-def status_pkgs(message):
-    status_notifier(chat_id=message.chat.id)
-
-
 @bot.message_handler(commands=['help'])
-def status_pkgs(message):
+def cmd_help(message):
     bot.send_message(message.chat.id, constants.HELP_INSTRUCTIONS)
-
-
-@bot.message_handler(commands=['stopnotifier'])
-def stop_notifier(message):
-    schedule.clear(message.chat.id)
-    bot.send_message(message.chat.id, constants.NOTIFIER_STOPPED, parse_mode='HTML')
-
-
-@bot.message_handler(commands=['startnotifier'])
-def start_notifier(message):
-    notifier(message)
-    bot.send_message(message.chat.id, constants.NOTIFIER_STARTED, parse_mode='HTML')
 
 
 # default handler for every other text
@@ -129,36 +106,6 @@ def start_notifier(message):
 def command_default(m):
     # this is the standard reply to a normal message
     bot.send_message(m.chat.id, "I don't understand \"" + m.text + "\"\nMaybe try the help page at /help")
-
-
-def status_notifier(chat_id) -> None:
-    pkgs = dbConfig().get_pkgs(int(chat_id))
-    if len(pkgs) <= 0:
-        bot.send_message(chat_id, "You have no apps yet, please use the command /addapp the add a new app.")
-    else:
-        for pkg in pkgs:
-            if scrap_app(pkg, chat_id) is not False:
-                bot.send_message(chat_id, f'🟢 {pkg}')
-            else:
-                bot.send_message(chat_id, f'🔴 {pkg}')
-
-
-def revenue_notifier(chat_id):
-    alm = applovinMax(applovin_report_key)
-    today_revenue = alm.today_revenue()
-    yesterday_revenue = alm.yesterday_revenue()
-    if today_revenue and yesterday_revenue:
-        today_revenue = round(float(today_revenue), 2)
-        yesterday_revenue = round(float(yesterday_revenue), 2)
-    bot.send_message(chat_id, f'💰Yesterday💰: <b>{yesterday_revenue}$</b>'
-                              f' \n\n💰<b>Today</b>💰: <b>{today_revenue}$</b>',
-                     parse_mode='HTML')
-
-
-def notifier(message):
-    if not schedule.get_jobs(message.chat.id):
-        schedule.every(1).hour.do(status_notifier, message.chat.id).tag(message.chat.id)
-        schedule.every(30).minutes.do(revenue_notifier, message.chat.id).tag(message.chat.id)
 
 
 bot.add_custom_filter(pkgsCallbackFilter())
